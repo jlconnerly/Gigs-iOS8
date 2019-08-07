eeea21bb637498755dea48dc321d8a4b3a2cf816
Jake <jlconnerly@gmail.com>
2019-08-07 15:03:24 -0500

    UI Set up complete.  Restructured files to fit mvc

---
+++ Gigs/Gigs/View Controllers/GigsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GigsViewController.swift
+//  Gigs
+//
+//  Created by Jake Connerly on 8/7/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class GigsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
